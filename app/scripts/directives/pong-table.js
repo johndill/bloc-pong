8986a055733fb0d6943e8b9712ee38ae7bcb0fa5
John Dill <dill.john.d@gmail.com>
2014-08-02 16:48:50 -0400

    added collision detection

--- app/scripts/directives/pong-table.js
+++ app/scripts/directives/pong-table.js
@@ -57,6 +57,57 @@ angular.module('blocPongApp')
   			context.fill();
   		};
 
+  		Ball.prototype.update = function(leftPaddle, rightPaddle, tableWidth, tableHeight) {
+  			this.x += this.x_speed;
+  			this.y += this.y_speed;
+
+  			var ballLeft = this.x - this.radius,
+  				ballRight = this.x + this.radius,
+  				ballTop = this.y + this.radius,
+  				ballBottom = this.y - this.radius,
+  				leftPaddleTop = leftPaddle.y,
+  				leftPaddleBottom = leftPaddle.y + leftPaddle.height,
+  				leftPaddleFace = leftPaddle.x + leftPaddle.width,
+  				rightPaddleTop = rightPaddle.y,
+  				rightPaddleBottom = rightPaddle.y + rightPaddle.height,
+  				rightPaddleFace = rightPaddle.x;
+
+  			// check if hitting top
+  			if (ballTop < 0) {
+  				this.x = this.radius;
+  				this.x_speed = -this.x_speed;
+  			}
+
+  			// check if hitting bottom
+  			if (ballBottom > tableHeight) {
+  				this.x = tableHeight - this.radius;
+  				this.x_speed = -this.x_speed;
+  			}
+
+  			// check if hitting left or right wall (point scored)
+  			if (ballLeft < 0 || ballRight > tableWidth) {
+  				this.x_speed = 3;
+  				this.y_speed = 0;
+  				this.x = tableWidth / 2;
+  				this.y = tableHeight / 2;
+  			}
+
+  			// check if left paddle was hit
+  			if (ballLeft < leftPaddleFace && this.y > leftPaddleTop && this.y < leftPaddleBottom) {
+  				this.x_speed = 3;
+  				this.y_speed += (leftPaddle.y_speed / 2);
+  				this.x += this.x_speed;
+  			}
+
+  			// check if right paddle was hit
+  			if (ballRight > rightPaddleFace && this.y > rightPaddleTop && this.y < rightPaddleBottom) {
+  				this.x_speed = -3;
+  				this.y_speed += (rightPaddle.y_speed / 2);
+  				this.x += this.x_speed;
+  			}
+
+  		};
+
   		var player = new Player();
   		var computer = new Computer();
   		var ball = new Ball(300, 200);
@@ -64,7 +115,10 @@ angular.module('blocPongApp')
 	    return {
 	    	restrict: 'A',
 	    	link: function(scope, element) {
-	    		var context = element[0].getContext('2d');
+	    		var context = element[0].getContext('2d'),
+	    			width = element.prop('width'),
+	    			height = element.prop('height');
+	    		 
 
 	    		var step = function() {
 	    			update();
@@ -73,12 +127,12 @@ angular.module('blocPongApp')
 	    		};
 
 	    		var update = function() {
-
+	    			ball.update(computer.paddle, player.paddle, width, height);
 	    		};
 
 	    		var render = function() {
 	    			context.fillStyle = '#FF00FF';
-	    			context.fillRect(0, 0, element.prop('width'), element.prop('height'));
+	    			context.fillRect(0, 0, width, height);
 	    			player.render(context);
 	    			computer.render(context);
 	    			ball.render(context);
