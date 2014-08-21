be9c8e58958580be0bc58db9a2c33f9d18278118
John Dill <dill.john.d@gmail.com>
2014-08-20 20:52:10 -0400

    all nodes on screen and moving around

--- app/scripts/directives/pong-table.js
+++ app/scripts/directives/pong-table.js
@@ -1,223 +1,226 @@
 'use strict';
 
+/*global d3*/
+
 angular.module('blocPongApp')
-  .directive('pongTable',
-  	function () {
-  		var animate = window.requestAnimationFrame ||
-  			window.webkitRequestAnimationFrame ||
-  			window.mosRequestAnimationFrame ||
-  			function (callback) { window.setTimeout(callback, 1000/60) };
+  .directive('pongTable', ['$log', function ($log) {
+    var table = function() {
+  		var width = 600, //window.innerWidth,  // width of game board
+        height = 400,//window.innerHeight,  // height of game board
+        unit = { width: width / 20, height: height / 15 },  // normalize game to different screens
+        keysDown = {},  // object that holds pressed keys
+        acceleration = 0, // vertical acceleration of y paddle
+        color = d3.scale.category10(),  // d3 color pallete
+        nodes = [], // array of nodes for d3 force layout
+        force = d3.layout.force(),   // d3 force layout
+        svg = d3.select('.pong-table-container').append('svg'),  // d3 svg element
+        nodeDrag = d3.behavior.drag().on('drag', function(d) {
+          // d3 node drag behavior
+          d.px += d3.event.dx;
+          d.py += d3.event.dy;
+          d.x += d3.event.dx;
+          d.y += d3.event.dy;
+        }),
+        bounds = function(rect) {
+          // calculates boundaries of rectangle from x, y, width, height and origin
+          // where origin is reference point in parent coortinate system
+          return {
+            left: rect.x - rect.width * rect.origin.x,
+            right: rect.x + rect.width * (1 - rect.origin.x),
+            top: rect.y - rect.height * rect.origin.y,
+            bottom: rect.y + rect.height * (1 - rect.origin.y)
+          };
+        },
+        PongNode = function(a) {
+          var args = a || {};
+            return {
+            x: args.x || 0,
+            y: args.y || 0,
+            width: args.width || unit.width,
+            height: args.height || unit.height,
+            px: args.px || 0,
+            py: args.py || 0,
+            rx: args.rx || 0,
+            ry: args.ry || 0,
+            origin: args.origin || { x: 0.5, y: 0.5 },
+            constrain: args.constrain || { x: false, y: false },
+            fixed: args.fixed || false,
+            name: args.name || 'pong-node',
+            bounds: function() { return bounds(this); }
+          };
+        },
+        plusOrMinus = function() {
+          return Math.random() < 0.5 ? -1 : 1;
+        },
+        ball = new PongNode({
+          x: width / 2,
+          y: height / 2,
+          px: width / 2 + unit.width / 2,
+          py: height / 2 + parseInt(Math.random()*15, 10)-7,
+          rx: unit.width / 2,
+          ry: unit.height / 2,
+          name: 'ball'
+        }),
+        leftPaddle = new PongNode({
+          x: unit.width,
+          y: height / 2,
+          height: 3 * unit.height,
+          width: unit.width,
+          px: unit.width,
+          py: height / 2,
+          constrain: { x: unit.width, y: false },
+          name: 'leftPaddle'
+        }),
+        rightPaddle = new PongNode({
+          x: width - unit.width,
+          y: height / 2,
+          height: 3 * unit.height,
+          width: unit.width,
+          px: width - unit.width,
+          py: height / 2,
+          constrain: { x: width - unit.width, y: false },
+          //fixed: true,
+          name: 'rightPaddle'
+        });
 
-  		// paddle
-  		function Paddle(x, y, width, height) {
-  			this.x = x;
-  			this.y = y;
-  			this.width = width;
-  			this.height = height;
-  			this.x_speed = 0;
-  			this.y_speed = 0;
-  		}
+      ball.init = function() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.px = width / 2 + plusOrMinus() * unit.width / 2;
+        this.py = height / 2 + plusOrMinus() * parseInt(Math.random()*5);
+      };
 
-  		Paddle.prototype.render = function(context) {
-  			context.fillStyle = '#0000FF';
-  			context.fillRect(this.x, this.y, this.width, this.height);
-  		};
+      // add nodes
+      nodes.push(ball, leftPaddle, rightPaddle);
+      $log.log(nodes); 
 
-  		Paddle.prototype.move = function(x, y, tableHeight) {
-  			var paddleTop, paddleBottom;
+      // configure force layout
+      force.gravity(0)
+        .friction(1)
+        .charge(function(d, i) { return i ? 0 : 100 * unit.width; })
+        .nodes(nodes)
+        .size([width, height])
+        .on('tick', function() {
+          collide();
+          svg.selectAll('rect')
+            .attr('x', function(d) { return d.bounds().left; })
+            .attr('y', function(d) { return d.bounds().top; });
+        })
+        .start();
 
-  			// update speed and location
-  			this.x += x;
-  			this.y += y;
-  			this.x_speed = x;
-  			this.y_speed = y;
-  			paddleTop = this.y;
-  			paddleBottom = this.y + this.height;
+      // convfigure svg
+      svg.attr('width', width)
+        .attr('height', height)
+        .attr('class', 'pong-table');
 
-  			// paddle at top
-  			if (paddleTop < 0) {
-  				this.y = 0;
-  				this.y_speed = 0;
-  			}
+      // add nodes to svg
+      svg.selectAll('rect')
+        .data(nodes)
+        .enter().append('rect')
+        .attr('width', function(d) { return d.width; })
+        .attr('height', function(d) { return d.height; })
+        .attr('rx', function(d) { return d.rx; })
+        .attr('ry', function(d) { return d.ry; })
+        .style('fill', function(d, i) { return color(i % 5); })
+        .attr('class', function(d) { return d.name; });
 
-  			// paddle at bottom
-  			if (paddleBottom > tableHeight) {
-  				this.y = tableHeight - this.height;
-  				this.y_speed = 0;
-  			}
-  		};
+      // add drag behavior to right paddle
+      d3.select('.rightPaddle').call(nodeDrag);
 
-  		// player
-  		function Player() {
-  			this.paddle = new Paddle(580, 175, 10, 50);
-  		}
-
-  		Player.prototype.render = function(context) {
-  			this.paddle.render(context);
-  		};
-
-  		Player.prototype.update = function(tableHeight) {
-  			var upArrow = 38, 
-  				downArrow  = 40,
-  				key,
-  				value;
-
-  			// paddle is not moving unless key is pressed
-  			this.paddle.x_speed = 0;
-  			this.paddle.y_speed = 0;
-
-  			// move paddle if key is pressed
-  			for (key in keysDown) {
-  				value = Number(key);
-  				if (value === upArrow) {
-  					this.paddle.move(0, -4, tableHeight);
-  				}
-  				else if (value === downArrow) {
-  					this.paddle.move(0, 4, tableHeight);
-  				}
-  			}
-  		};
-
-  		// computer
-  		function Computer() {
-  			this.paddle = new Paddle(10, 175, 10, 50);
-  		}
-
-  		Computer.prototype.render = function(context) {
-  			this.paddle.render(context);
-  		};
-
-  		Computer.prototype.update = function(ball) {
-  			var paddleCenter = this.paddle.y + (this.paddle.height / 2),
-  				diff = ball.y - paddleCenter,
-  				speed;
-
-  			// set speed
-  			if (diff < -3) {
-  				speed = -4;  // max speed up
-  			}
-  			else if (diff > 3) {
-  				speed = 4;	// max speed down
-  			}
-  			else {
-  				speed = diff;  // move faster if farthur away
-  			}
-
-  			// move paddle
-  			this.paddle.move(0, speed);
-  		}
-
-  		// ball
-  		function Ball(x, y) {
-  			this.x = x;
-  			this.y = y;
-  			this.x_speed = 3;
-  			this.y_speed = 0;
-  			this.radius = 5;
-  		}
-
-  		Ball.prototype.render = function(context) {
-  			context.beginPath();
-  			context.arc(this.x, this.y, this.radius, 2* Math.PI, false);
-  			context.fillStyle = '#000000';
-  			context.fill();
-  		};
-
-  		Ball.prototype.update = function(leftPaddle, rightPaddle, tableWidth, tableHeight) {
-  			var ballLeft = this.x - this.radius,
-  				ballRight = this.x + this.radius,
-  				ballTop = this.y - this.radius,
-  				ballBottom = this.y + this.radius,
-  				leftPaddleTop = leftPaddle.y,
-  				leftPaddleBottom = leftPaddle.y + leftPaddle.height,
-  				leftPaddleFace = leftPaddle.x + leftPaddle.width,
-  				rightPaddleTop = rightPaddle.y,
-  				rightPaddleBottom = rightPaddle.y + rightPaddle.height,
-  				rightPaddleFace = rightPaddle.x;
-
-  			// update ball position
-  			this.x += this.x_speed;
-  			this.y += this.y_speed;
-
-  			// check if hitting top
-  			if (ballTop < 0) {
-  				this.y = this.radius;
-  				this.y_speed = -this.y_speed;
-  			}
-
-  			// check if hitting bottom
-  			if (ballBottom > tableHeight) {
-  				this.y = tableHeight - this.radius;
-  				this.y_speed = -this.y_speed;
-  			}
-
-  			// check if hitting left or right wall (point scored)
-  			if (ballLeft < 0 || ballRight > tableWidth) {
-  				this.x_speed = 3;
-  				this.y_speed = 0;
-  				this.x = tableWidth / 2;
-  				this.y = tableHeight / 2;
-  			}
-
-  			// check if left paddle was hit
-  			else if (ballLeft < leftPaddleFace && this.y > leftPaddleTop && this.y < leftPaddleBottom) {
-  				this.x_speed = 3;
-  				this.y_speed += (leftPaddle.y_speed / 4);
-  				this.x += this.x_speed;
-  			}
-
-  			// check if right paddle was hit
-  			else if (ballRight > rightPaddleFace && this.y > rightPaddleTop && this.y < rightPaddleBottom) {
-  				this.x_speed = -3;
-  				this.y_speed += (rightPaddle.y_speed / 4);
-  				this.x += this.x_speed;
-  			}
-
-  		};
-
-  		var player = new Player();
-  		var computer = new Computer();
-  		var ball = new Ball(300, 200);
-  		var keysDown = {};
-
+      // listen for keys pressed
   		window.addEventListener('keydown', function(event) {
   			keysDown[event.keyCode] = true;
   		});
 
   		window.addEventListener('keyup', function(event) {
   			delete keysDown[event.keyCode];
+        acceleration = 0;
   		});
 
-	    return {
-	    	restrict: 'A',
-	    	link: function(scope, element) {
-	    		var context = element[0].getContext('2d'),
-	    			width = element.prop('width'),
-	    			height = element.prop('height')
-	    		 
-	    		var step = function() {
-	    			update();
-	    			render();
-	    			animate(step);
-	    		};
+      // analyze position of each node and process any collisions
+      function collide() {
+        var i = 0,
+            n = nodes.length,
+            dy,
+            max;
 
-	    		var update = function() {
-	    			player.update(height);
-	    			computer.update(ball);
-	    			ball.update(computer.paddle, player.paddle, width, height);
-	    		};
+        // move player paddle based on keyboard input
+        if (keysDown[38]) {
+          acceleration += 1;
+          rightPaddle.py -= 3 + acceleration;
+        }
+        if (keysDown[40]) {
+          acceleration += 1;
+          rightPaddle.py += 3 + acceleration;
+        }
 
-	    		var render = function() {
-	    			context.fillStyle = '#FF00FF';
-	    			context.fillRect(0, 0, width, height);
-	    			player.render(context);
-	    			computer.render(context);
-	    			ball.render(context);
-	    		};
+        // loop through all nodes and apply constraints
+        while (++i < n) {
+          if (nodes[i].constrain.x) {
+            nodes[i].x = nodes[i].constrain.x;
+            nodes[i].px = nodes[i].constrain.x;
+          }
+          if (nodes[i].constrain.y) {
+            nodes[i].y = nodes[i].constrain.y;
+            nodes[i].py = nodes[i].constrain.y;
+          }
 
-	    		// start animation loop
-	    		animate(step);
-	    	}
-	    };
-	  }
-  );
+          // top and bottom wall
+          if (nodes[i].bounds().top < 0) {
+            nodes[i].y = nodes[i].height / 2;
+            nodes[i].py = nodes[i].height / 2;
+          }
+          if (nodes[i].bounds().bottom > height) {
+            nodes[i].y = height - nodes[i].height / 2;
+            nodes[i].py = height - nodes[i].height / 2; 
+          }
+        }
+              
+        // ball hits left paddle
+        if (ball.bounds().left < leftPaddle.bounds().right) {
+          dy = ball.y - leftPaddle.y;
+          max = leftPaddle.bounds().top + ball.height / 2;
+          if (Math.abs(dy) <= max) {
+            ball.px = ball.x - unit.width / 2;
+            ball.py = ball.y - dy / max * unit.height;
+            force.resume();
+          }
+        }
+
+        // ball hits right paddle
+        if (ball.bounds().right > rightPaddle.bounds().left) {
+          dy = ball.y - rightPaddle.y;
+          max = rightPaddle.bounds().top + ball.height / 2;
+          if (Math.abs(dy) <= max) {
+            ball.px = ball.x + unit.width / 2;
+            ball.py = ball.y - dy / max * unit.height;
+            force.resume();
+          }
+        }
+
+        // ball hits top or bottom wall
+        if (ball.bounds().top < 0 || ball.bounds().bottom > height) {
+          ball.py = ball.y + (ball.y - ball.py);
+          force.resume();
+        }
+        
+        // score - ball hits left or right wall
+        if (ball.bounds().left < 0) {
+          ball.init();
+        }
+        if (ball.bounds().right > width) {
+          ball.init();
+        }
+        
+      }
+    };
+
+    return {
+    	restrict: 'A',
+      replace: false,
+    	link: function() {
+    		// start force layout
+    		table();
+    	}
+    };
+  }]);
